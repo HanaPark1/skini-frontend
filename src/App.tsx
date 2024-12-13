fbb0bc0ed1899c9cec2e80262dad998823f632ea
HanaPark1 <ydjxlst@gmail.com>
2024-12-13 20:30:23 +0900

    feat: 깃허브 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import styled from "@emotion/styled";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import InfoPage from "./page/Info/InfoPage"
 import HomePage from "./page/Home/HomePage"
 import LoginPage from "./page/Login/LoginPage";
 import SignUpPage from "./page/SignUp/SignUpPage";
-import Info from "./page/SignUp/SignUpPage.Info";
 import Info2 from "./page/SignUp/SignUpPage.Info2";
 import Success from "./page/SignUp/SignUpPage.Success";
-import FindMainPage from "./page/FindIDnPW/FindMainPage";
 import DiagnosisUproadPage from "./page/Diagnosis/DiagnosisUproadPage";
 import DiagnosisDetailPage from "./page/Diagnosis/DiagnosisDetailPage";
 import NearestHospital from "./page/Map/NearestHospitalPage";
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react(),],
+	base: '/skini-frontend/',
 	server: {
 		port: 3000,
 		proxy: {
